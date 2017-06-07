85639ee6b90be2a089fe86ff37c26c1ef5be1390
jannessantoso <jannes.sa@tnis.com>
2017-06-07 19:21:48 +0700

    update framework

--- src/customer/thirdparty/rabbitmq.go
+++ src/customer/thirdparty/rabbitmq.go
@@ -1,19 +1,20 @@
 package thirdparty
 
-import(
-	"github.com/streadway/amqp"
- 	"github.com/astaxie/beego"
+import (
 	"encoding/json"
 	"os"
+
+	"github.com/astaxie/beego"
+	"github.com/streadway/amqp"
 )
 
 // TypeLogData = request Log Data
 type TypeLogData struct {
-	From string `json:"from"`
-	To string `json:"to"`
-	ReqID string `json:"reqId"`
-	Header string `json:"header"`
-	Body string `json:"body"`
+	From    string `json:"from"`
+	To      string `json:"to"`
+	ReqID   string `json:"reqId"`
+	Header  string `json:"header"`
+	Body    string `json:"body"`
 	TypeRel string `json:"type"`
 }
 
@@ -23,20 +24,27 @@ func CredMq() string {
 	envOs := os.Getenv("GOENV")
 	if envOs == "local" {
 		mq = beego.AppConfig.String("mq::local")
-	}else if envOs == "dev" {
+	} else if envOs == "dev" {
 		mq = beego.AppConfig.String("mq::dev")
-	}else if envOs == "prod" {
+	} else if envOs == "prod" {
 		mq = beego.AppConfig.String("mq::prod")
 	}
 
 	return mq
 }
 
-// SendMq = to Send MQ Data
-func SendMq(inputReqBody []byte,fromService string,toService string,headerAll string,reqID string,newRequest bool,typeRelation string) {
- 	mq := CredMq()
+func ConnectMq(mq string) (*amqp.Connection, error) {
+	conn, err := amqp.Dial(mq)
+	CheckErr(err, "Failed to connect to RabbitMQ")
 
- 	conn, err := amqp.Dial(mq)
+	return conn, err
+}
+
+// SendMq = to Send MQ Data
+func SendMq(inputReqBody []byte, fromService string, toService string, headerAll string, reqID string, newRequest bool, typeRelation string) {
+	mq := CredMq()
+
+	conn, err := ConnectMq(mq)
 	CheckErr(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
@@ -45,25 +53,26 @@ func SendMq(inputReqBody []byte,fromService string,toService string,headerAll st
 	defer ch.Close()
 
 	q, err := ch.QueueDeclare(
-		"log",	// name
-		false,	// durable
-		false,	// delete when unused
-		false,	// exclusive
-		false,	// no-wait
-		nil,	// arguments
+		"log", // name
+		false, // durable
+		false, // delete when unused
+		false, // exclusive
+		false, // no-wait
+		nil,   // arguments
 	)
 	CheckErr(err, "Failed to declare a queue")
 
 	typeLogData := &TypeLogData{
-		From:fromService,
-		To:toService,
-		ReqID:reqID,
-		Header:headerAll,
-		Body:string(inputReqBody),
-		TypeRel:typeRelation,
+		From:    fromService,
+		To:      toService,
+		ReqID:   reqID,
+		Header:  headerAll,
+		Body:    string(inputReqBody),
+		TypeRel: typeRelation,
 	}
-	logJSONMarshal,err := json.Marshal(typeLogData);CheckErr(err,"error rabbitMQ line 47")
-	
+	logJSONMarshal, err := json.Marshal(typeLogData)
+	CheckErr(err, "error rabbitMQ line 47")
+
 	err = ch.Publish(
 		"",     // exchange
 		q.Name, // routing key
@@ -82,7 +91,7 @@ func SendMq(inputReqBody []byte,fromService string,toService string,headerAll st
 // CheckErr = Checking Error
 func CheckErr(err error, msg string) {
 	if err != nil {
-	    beego.Warning("MESSAGE RABBITMQ: "+msg)
-	    beego.Error(1024,err)
+		beego.Warning("MESSAGE RABBITMQ: " + msg)
+		beego.Error(1024, err)
 	}
 }
